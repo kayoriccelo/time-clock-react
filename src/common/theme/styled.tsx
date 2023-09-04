5c5d7787960027f7f758ca36999a0ed355e4c75b
kayoriccelo <kayo.desenvolvimento@gmail.com>
2023-09-04 12:57:46 -0300

    feat: created a new common style file.

---
+++ src/common/theme/styled.tsx
@@ -0,0 +1,36 @@
+const fontFamily = ''
+const fontSize = ''
+
+const primaryLight =  ''
+const successLight = ''
+const warningLight =  ''
+const infoLight =  ''
+const dangerLight =  ''
+const secondaryLight =  ''
+const neutralLight =  ''
+
+const primaryDark =  ''
+const successDark = ''
+const warningDark =  ''
+const infoDark =  ''
+const dangerDark =  ''
+const secondaryDark =  ''
+const neutralDark =  ''
+
+export const theme = {
+    light: {
+     
+    },
+    
+    dark: {
+
+    }
+}
+
+export const rows = {
+
+}
+
+export const cols = {
+
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './pages/app'
+import './common/theme/style.css'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
