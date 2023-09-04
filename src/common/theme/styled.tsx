5e29ae4cdbe5e624e2afcf85fbaec747e0d69d48
kayoriccelo <kayo.desenvolvimento@gmail.com>
2023-09-04 13:03:05 -0300

    feat: added new background style to theme.

--- src/common/theme/styled.tsx
+++ src/common/theme/styled.tsx
@@ -1,30 +1,33 @@
 const fontFamily = ''
 const fontSize = ''
 
-const primaryLight =  ''
+const primaryLight = ''
 const successLight = ''
-const warningLight =  ''
-const infoLight =  ''
-const dangerLight =  ''
-const secondaryLight =  ''
-const neutralLight =  ''
+const warningLight = ''
+const infoLight = ''
+const dangerLight = ''
+const secondaryLight = ''
+const neutralLight = ''
 
-const primaryDark =  ''
+const primaryDark = ''
 const successDark = ''
-const warningDark =  ''
-const infoDark =  ''
-const dangerDark =  ''
-const secondaryDark =  ''
-const neutralDark =  ''
+const warningDark = ''
+const infoDark = ''
+const dangerDark = ''
+const secondaryDark = ''
+const neutralDark = ''
 
 export const theme = {
     light: {
-     
+        background: `
+            background-color: rgb(241, 245, 249);
+        `,
     },
-    
     dark: {
-
-    }
+        background: `
+            background-color: rgb(21, 23, 30);
+        `,
+    },
 }
 
 export const rows = {
