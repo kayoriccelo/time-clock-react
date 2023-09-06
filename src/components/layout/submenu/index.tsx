0294a7a8ec9ca77ae986ec746dca2d58499fa85a
kayoriccelo <kayo.desenvolvimento@gmail.com>
2023-09-06 08:28:45 -0300

    refactor: added theme in Submenu component

--- src/components/layout/submenu/index.tsx
+++ src/components/layout/submenu/index.tsx
@@ -1,15 +1,18 @@
+import { useContext } from "react"
+import ThemeContext from "../../../pages/app/contexts"
 import { SubMenuProps } from "./interface"
 import { SubMenuStyled } from "./style/styled"
 
 
 export function SubMenu({ title, to, icon, open, handleOpen }: SubMenuProps) {
+    const { theme } = useContext(ThemeContext)
 
     function handleClick() {
         handleOpen && handleOpen(!open)
     }
 
     return (
-        <SubMenuStyled to={to} onClick={handleClick}>
+        <SubMenuStyled to={to} theme={theme} onClick={handleClick}>
             {icon && icon}
 
             {icon && title}
