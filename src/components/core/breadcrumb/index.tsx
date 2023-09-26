2daf07944a62aeb514078efa3a40a0a569bc4357
kayoriccelo <kayo.desenvolvimento@gmail.com>
2023-09-26 12:43:07 -0300

    refactor: added new click action in Breadcrumb component.

--- src/components/core/breadcrumb/index.tsx
+++ src/components/core/breadcrumb/index.tsx
@@ -1,4 +1,5 @@
 import { useContext } from "react"
+import { useNavigate } from "react-router-dom"
 import { TbPointFilled } from "react-icons/tb"
 
 import { GlobalContext } from "../../../pages/app/contexts"
@@ -7,17 +8,33 @@ import { SBreadcrumb, SBreadcrumbIcon, SBreadcrumbLabel } from "./styles/styled"
 
 
 export function Breadcrumb() {
-    const { global } = useContext(GlobalContext)
+    const navigate = useNavigate()
+    const { global, setGlobal } = useContext(GlobalContext)
     const { breadcrumbs } = global
+
+    function handlerClick(breadcrumb: any) {
+        navigate(breadcrumb.to)
+
+        setGlobal({
+            ...global,
+            title: breadcrumb.title,
+            breadcrumbs: breadcrumb.previous
+        })
+    }
 
     return (
         <SBreadcrumb>
             {breadcrumbs && breadcrumbs.map((breadcrumb, index) => {
                 return (
-                    <div key={`bf-${index}`} style={{display: 'flex'}}>
-                        <SBreadcrumbIcon key={`sb-icon-${index}`}><TbPointFilled key={`b-icon-${index}`}/></SBreadcrumbIcon>
+                    <div key={`bf-${index}`} style={{ display: 'flex' }}>
+                        <SBreadcrumbIcon key={`sb-icon-${index}`}>
+                            <TbPointFilled key={`b-icon-${index}`} />
+                        </SBreadcrumbIcon>
 
-                        <SBreadcrumbLabel key={`sb-label-${index}`}>
+                        <SBreadcrumbLabel
+                            key={`sb-label-${index}`}
+                            onClick={() => handlerClick(breadcrumb)}
+                        >
                             {breadcrumb.title}
                         </SBreadcrumbLabel>
                     </div>
