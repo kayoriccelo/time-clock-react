58362504a720e15add9c8029d07b223cc211335e
kayoriccelo <kayo.desenvolvimento@gmail.com>
2024-02-01 19:00:43 -0300

    refactor: added group in pagination component.

--- src/components/core/pagination/styles/index.ts
+++ src/components/core/pagination/styles/index.ts
@@ -2,13 +2,32 @@ import styled from "styled-components"
 
 
 export const SPagination = styled.div`
+    ${({ theme: { components: { core: { pagination } } } }) => pagination.style}
+    max-width: 30rem;
+
+    @media (max-width: 600px) {
+        justify-content: center;
+        width: 100%;
+        box-shadow: none;
+    }
+`
+
+export const SPaginationGroup = styled.div`
     display: flex;
     align-items: center;
+    padding: 0.3rem;
+    margin-top: 0.6rem;
+    clip-path: polygon(0% 0, 100% 0%, 85% 100%, 0% 100%);
+    border-right: 10rem solid transparent;
+    border-top-right-radius: 1rem;
 
     ${({ theme: { components: { core: { pagination } } } }) => pagination.group.style}
 
     @media (max-width: 600px) {
         justify-content: center;
-        padding: 1rem;
+        clip-path: none;
+        border-right: none;
+        border-top-right-radius: none;
+        border-radius: 1rem;
     }
 `
