52982d0f6249d066a0707b5ca9a42b2e7a2a68e1
kayoriccelo <kayo.desenvolvimento@gmail.com>
2024-02-01 19:18:25 -0300

    feat: point marking page started.

---
+++ src/pages/point_marking/index.tsx
@@ -0,0 +1,14 @@
+import { Breadcrumb, Title } from "../../components"
+
+
+export function PointMarking() {
+    return (
+        <>
+            <Title />
+
+            <Breadcrumb />
+
+            <div>Point Marking</div>
+        </>
+    )
+}
