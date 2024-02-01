d1172936a2d5864c2ebc0d6b45854a5dbc56c0ad
kayoriccelo <kayo.desenvolvimento@gmail.com>
2024-02-01 19:15:46 -0300

    refactor: changed style of main layout component.

--- src/components/layout/main/styles/index.ts
+++ src/components/layout/main/styles/index.ts
@@ -2,10 +2,10 @@ import { styled } from "styled-components"
 
 
 export const SMain = styled.div`
-    flex: 14;
     display: flex;
     height: 100vh;
     flex-direction: column;
+    width: 100%;
 
     ${({ theme: { components: { layout: { main } } } }) => main.style}
 `
