c999d7ee03e60e58cc7cebcb48356f520652932e
kayoriccelo <kayo.desenvolvimento@gmail.com>
2024-02-01 19:13:26 -0300

    refactor: improvement in the layout content component.

--- src/components/layout/container/styles/index.tsx
+++ src/components/layout/container/styles/index.tsx
@@ -4,6 +4,6 @@ import { styled } from "styled-components"
 export const SContainer = styled.div`
     display: flex;
     height: 100vh;
-    
+        
     ${({ theme: { components: { layout: { container } } } }) => container.style}
 `
